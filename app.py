6fb4dbdd204ea7a20a321beb7bb3dac06779da33
anilkurmi <anilkurmi@gmail.com>
2026-08-01 21:50:26 +0545

    Fixed port binding for Render

--- app.py
+++ app.py
@@ -645,4 +645,7 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)import os
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
