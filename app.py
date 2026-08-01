a638c736873567a65770120c0116d9a529c2c716
anilkurmi <anilkurmi@gmail.com>
2026-08-01 21:54:22 +0545

    Fixed port binding properly

--- app.py
+++ app.py
@@ -645,7 +645,5 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)import os
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
